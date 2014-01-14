1cf679602a5dbaff673e0f9a84998fe8f6e8a866
Iain Sproat <iainsproat@gmail.com>
2014-01-14 19:14:39 +0000

    Test dynamic parameters

---
+++ Nancy-sandbox-query-46-test/MyModuleTest.cs
@@ -0,0 +1,35 @@
+﻿namespace Nancy_sandbox_query_46
+{
+    using System;
+
+    using Nancy;
+    using Nancy.Testing;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class MyModuleTest
+    {
+        Browser browser;
+
+        [SetUp]
+        public void SetUp()
+        {
+            browser = new Browser(with =>
+            {
+                with.Module<MyModule>();
+                with.EnableAutoRegistration();
+            });
+        }
+
+        [Test]
+        public void Can_Get_View()
+        {
+            // When
+            var result = browser.Get("/path/foobar/anotherAction", with => with.HttpRequest());
+
+            // Then
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+            Assert.AreEqual("Hello foobar", result.Body.AsString()); //fails as parameters.Name is always null when cast to a string
+        }
+    }
+}
---
+++ Nancy-sandbox-query-46/MyModule.cs
@@ -0,0 +1,38 @@
+﻿namespace Nancy_sandbox_query_46
+{
+    using System;
+
+    using Nancy;
+
+    public class MyModule : NancyModule
+    {
+        public MyModule()
+        {
+//            Get["/path/{Name}/action"] = parameters =>
+//                {
+//                    return MyMethod(parameters.Name, methodToBeCalled); // this does not compile              
+//                };
+
+            Get["/path/{Name}/anotherAction"] = parameters =>
+                {
+                    return MyMethod(parameters.Name as string, anotherMethodToBeCalled);
+                };
+        }
+
+        public Response MyMethod(string name, Func<int> doSomething)
+        {
+            doSomething();
+            return Response.AsText(string.Format("Hello {0}", name));
+        }
+
+        public int methodToBeCalled()
+        {
+            return -1;
+        }
+
+        public int anotherMethodToBeCalled()
+        {
+            return 1;
+        }
+    }
+}
